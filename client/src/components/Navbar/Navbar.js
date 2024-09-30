e8175711989a1778e901ea01149a843e2c1e53ef
John Doe <mahdibenalaya566@gmail.com>
2024-09-30 20:48:13 +0100

    nav

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -17,7 +17,8 @@ function Navbar() {
   {token? null:<>
      <Link to={"/login"}> <button type="button"  className="btn-gym ">signin</button> </Link>
      <Link to={"/signup"}><button type="button"  className="btn-gym">subscribe</button></Link></>}
-{token && user.role=="user" ? <Link to={"/profile"}><button type="button"  className="btn-gym l ">profile</button></Link>:null}
+{token && user.role=="user" && window.location.pathname=='/' ? <Link to={"/profile"}><button type="button"  className="btn-gym l ">profile</button></Link>:null}
+{token && user.role=="user" && window.location.pathname=='/profile'    ? <Link to={"/"}><button type="button"  className="btn-gym l ">Home</button></Link>:null}
 {token && user.role=="admin" ? <Link to={"/admin"}><button type="button"  className="btn-gym l ">admin profile</button></Link>:null}
  {token && user.role=="reception" ? <Link to={"/reception"}><button type="button"  className="btn-gym l">reception profile</button></Link>:null}
 {token ?<button onClick={() => disptach(logout(),naviagte("/"))}  type="button"  className="l btn-gym">logout</button>:null} 
